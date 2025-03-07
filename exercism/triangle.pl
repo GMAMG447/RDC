423e946d8cffec278cc8d8e5389325ad78404d9d
GMAMG447 <arian.madrigal@alu.uhu.es>
2025-03-07 13:27:58 +0100

    Create triangle.pl

---
+++ exercism/triangle.pl
@@ -0,0 +1,12 @@
+/*
+triangle(A, B, C, R).
+    es cierto si R unifica con "equilateral"
+    y A B C corresponden a la longitud de un triangulo equilatero.
+
+    es cierto si R unifica con "scalene"
+    y A B C corresponden a la longitud de un triangulo escaleno.
+
+    es cierto si R unifica con "isosceles"
+    y A B C corresponden a la longitud de un triangulo isosceles.
+
+    */
