63f7448a50e721b10e314d1e1a5dedf3393e18df
GMAMG447 <arian.madrigal@alu.uhu.es>
2025-03-13 09:55:06 +0100

    Update unificacion.pl

--- unificacion.pl
+++ unificacion.pl
@@ -33,6 +33,31 @@ my_Length([],0).
 
 my_Length([Cab|Resto], ) :- my_Lenght(Resto, R), R2 is R + 1.
 
+/*
 
+comprime(Lista, R)
+  es cierto si R unifica con una lista de La si siguiente forma:
+
+comprime([a,a,a,b,b,c,a,a], R).
+R=[(a,3),(b,2),(c,1),(a,2)]
+
+*/
+
+/*
+
+elemento_n(+Lista, +N, -R)
+  es cierto cuando R unifica con el elemento
+  de Lista que ocupa la posicion N empezando a contar
+  en 1.
+
+  Inducción
+  1) P(n0)
+  2) P(n-1) -> P(n)
+
+*/
+
+elemento_n([Cab|_], 1, Cab).
+
+elemento_n([Cab|Resto], N, ]) :- N2 is N-1, elemento_n(Resto, N2, R]), N > 1
 
 
