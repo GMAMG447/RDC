91422285f0ac7b84db5f4dce2da929f36b365d43
GMAMG447 <arian.madrigal@alu.uhu.es>
2025-03-13 09:05:22 +0100

    Update unificacion.pl

--- unificacion.pl
+++ unificacion.pl
@@ -1,4 +1,30 @@
 %        UNIFICACIÓN
 %
 %        
-%
+
+[Cabeza|Resto] = [1,2,3,4].
+%Cabeza = 1, Resto = [2,3,4]
+
+[Cabeza1,Cabeza2|Resto] = [1,2,3,4].
+%Cabeza = 1, Resto = [2,3,4]
+
+[Cabeza|Resto] = [1].
+%Cabeza = 1, Resto = []
+
+[Cabeza1,Cabeza2|Resto] = []. % Necesita al menos un elemento
+%false 
+
+
+/*
+
+my_Lenght(List,Num)
+  es cierto di Num unifica con
+  el número de elementos de List.
+
+Introducción
+1) P(n0) es cierta.
+2) P(n-1) -> P(n)
+
+*/
+
+my_length([],_).
