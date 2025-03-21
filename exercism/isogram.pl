bc62312abae8de3180d5d315e60c2c6e8fa94fb8
GMAMG447 <arian.madrigal@alu.uhu.es>
2025-03-21 13:56:39 +0100

    Update isogram.pl

--- exercism/isogram.pl
+++ exercism/isogram.pl
@@ -12,5 +12,30 @@
   six-year-old
   The word isograms, however, is not an isogram, because the s repeats.
 
+  Es cierto si la cadena contiene caracteres diferentes
+  excepto los espacios en blanco y guiones que se podran repetir.
 */
+
+/*
+quita_blancos(Lista,R)
+  es cierto si R unifica con la lista de numeros que
+  que contiene lista, eliminando las ocurrencias de 
+  32 y 45 (" " y "-").
+
+*/
+
+/*
+string_lower(+String, string)
+string_codes(String,codes)
+*/
+
+blanco(32).blanco(45).
+quita_blancos(L,R) :- exclude(blanco,C,R), string_lower(L,Low), string_codes(Low,C).
+
+isogram(S) :- all_distinct(R), quita_blancos(S,R).
+
+
+
+
+
 
