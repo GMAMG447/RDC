261e060b58acea74b80bb89ade4999c3fb08411a
GMAMG447 <arian.madrigal@alu.uhu.es>
2025-03-21 10:41:37 +0100

    Create sudoku.pl

---
+++ sudoku.pl
@@ -0,0 +1,27 @@
+/*
+
+sudoku(Matrix).
+  Es cierto si Matrix unifica con
+  una matriz 9x9 casillas, divido en 9 bloques
+  de 3x3 casillas, con los numeros del 1 añ 9,
+  cun numeros distintos por filas, distintos por
+  columnas y distintos por cada bloque 3x3.
+
+*/
+
+sudoku(Rows) :-
+        length(Rows, 9),
+        maplist(same_length(Rows), Rows),
+        append(Rows, Vs), Vs ins 1..9,
+        maplist(all_distinct, Rows),
+        transpose(Rows, Columns),
+        maplist(all_distinct, Columns),
+        Rows = [As,Bs,Cs,Ds,Es,Fs,Gs,Hs,Is],
+        blocks(As, Bs, Cs),
+        blocks(Ds, Es, Fs),
+        blocks(Gs, Hs, Is).
+
+blocks([], [], []).
+blocks([N1,N2,N3|Ns1], [N4,N5,N6|Ns2], [N7,N8,N9|Ns3]) :-
+        all_distinct([N1,N2,N3,N4,N5,N6,N7,N8,N9]),
+        blocks(Ns1, Ns2, Ns3).
