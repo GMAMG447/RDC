adce256a76a09dfb0a382e573296e1125c7a0a4b
GMAMG447 <arian.madrigal@alu.uhu.es>
2025-04-10 09:02:38 +0200

    Update burbuja.pl

--- burbuja.pl
+++ burbuja.pl
@@ -4,3 +4,22 @@
 %  contiene los mismos elementos que Lista ordenados
 %  de menor a mayor.
 %-----------------------------------------------------
+
+/*
+
+ordenada(Lista)
+  es cierto si los elementos de Lista están ordenados 
+  de menor a mayor.
+
+Principio de induccion
+1) P(n0)
+2) P(n-1) -> P(n) ==> P(N) :- N2 is N - 1, P(N2).
+
+*/
+
+ordenada([]).
+ordenada([_]).
+ordenada([Cab1|Cab2|Resto]) :- Cab1<Cab2, ordenada(Cab2|Resto). % Se ponen las restricciones como comparaciones delante.
+
+
+
