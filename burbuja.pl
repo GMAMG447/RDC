b6c4c257509b1370fe114b58cb14a58038fe3076
GMAMG447 <arian.madrigal@alu.uhu.es>
2025-04-10 08:49:07 +0200

    Create burbuja.pl

---
+++ burbuja.pl
@@ -0,0 +1,6 @@
+%-----------------------------------------------------
+% ordena_burbuja(+Lista, -ListaR).
+%  es cierto cuando ListaR unifica con una lista que
+%  contiene los mismos elementos que Lista ordenados
+%  de menor a mayor.
+%-----------------------------------------------------
