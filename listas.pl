b877786cf14d36249854479330a268b35c22594b
GMAMG447 <arian.madrigal@alu.uhu.es>
2025-04-24 09:52:51 +0200

    Update listas.pl

--- listas.pl
+++ listas.pl
@@ -17,3 +17,24 @@ inserta_en_list_ord(Elem,[],[Elem]).
 inserta_en_list_ord(Elem, [Cab|Resto], [Elem,Cab|Resto]) :- Elem =< Cab.
 
 inserta_en_list_ord(Elem, [Cab|Resto], [Cab|R]) :- Elem > Cab, inserta_en_list_ord(Elem, Resto, R).
+
+/*
+
+ordena_quick(+Lista, -ListaR)
+  Es cierto cuando ListaR unifica con una lista que
+  contiene los mismos elementos que Lista ordenados
+  de menor a mayor.
+
+*/
+
+ordena_quick([],[]).
+
+ordena_quick([Cab|Resto], ) :- divide(Cab, Resto, Men, May), ordena_quick(Men,RMen), ordena_quick(May,RMay), append(RMen, [Cab|RMay], R).
+
+divide(_,[],[],[]).
+
+divide(Elem,[Cab,Resto], [Cab|Men], May) :- divide(Elem, Resto, Men, May), Cab =< Elem.
+
+divide(Elem,[Cab,Resto], Men, [Cab|May]) :- divide(Elem, Resto, Men, May), Cab > Elem.
+
+
