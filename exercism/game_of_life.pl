5c2b6673868490e7b27933cae249a18c3c0434b3
GMAMG447 <arian.madrigal@alu.uhu.es>
2025-04-25 14:33:29 +0200

    Update game_of_life.pl

--- exercism/game_of_life.pl
+++ exercism/game_of_life.pl
@@ -8,3 +8,5 @@ neighbours(X,Y,Matrix,List):- select(Matrix,Y,Rows),
     transpose(Rows, Cols), select(Cols,X,R), transpose(R,R2), append(R2, List).
 
     
+% nth0(X, [5,4,3], 5). --> X = 0 (primera posicion).
+% nth0(0, [5,4,3], X). --> X = 5 (primer elemento).
