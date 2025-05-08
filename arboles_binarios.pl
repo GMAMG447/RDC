0c1430e92957ab9f0f97679f6ff1f49288852718
GMAMG447 <arian.madrigal@alu.uhu.es>
2025-05-08 09:38:42 +0200

    Update arboles_binarios.pl

--- arboles_binarios.pl
+++ arboles_binarios.pl
@@ -41,22 +41,46 @@ crea_ab(Lista, ArbolBinario)
   balanceado que contiene todas las etiquetas de Lista.
   
 balanceado(Arbol)
-  Es cierto si <<para todo nodo>> de Arbol la diferencia entre
+  Es cierto si para todo nodo de Arbol la diferencia entre
   la altura del subarbol izq y la del derecho sea como maximo 1.
   
 */
 
 crea_ab([], nil).
 
-crea_ab([Cab | Resto] , a(Cab, A1, A2)) :-
+crea_ab([Cab|Resto], a(Cab, A1, A2)) :-
   length(Resto, I),
   Med is L div 2,
   length(L1, Med), /* Crea una lista de la mitad de longitud de Lista */
   append(L1, L2, Resto),
   crea_ab(L1, A1),
   crea_ab(L2, A2).
-
-
   
+/*
+altura(ArbolBin, Alt)
+  Es cierto si Alt unifica con la altura del ArbolBin.
+
+*/
+  
+altura(nil,0).
+
+altura(a(Cab,A1,A2), Alt) :-
+  altura(A1, Alt1),
+  altura(A2, Alt2),
+  Alt1 < Alt2
+  Alt is Alt2 + 1.
+
+altura(a(Cab,A1,A2), Alt) :-
+  altura(A1, Alt1),
+  altura(A2, Alt2),
+  Alt1 >= Alt2
+  Alt is Alt1 + 1.
+
+balanceado(a(_, AI, AD)) :-
+  altura(AI,ai),
+  altura(AD,ad),
+  abs(ai - ad) <= 1.
+
+
   
   
